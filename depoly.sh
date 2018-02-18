126ab7a44664c010fea129a78c7f5a1f6d7e2d0f
John Kasper Svergja <john.k.svergja@gmail.com>
2018-02-18 15:04:14 +0100

    Updated deploy script

--- depoly.sh
+++ depoly.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 id=$(ps -ef | awk '/[j]etty/{print $2}')
 if [ -n "$id" ]; then
 	echo "Killing jetty with PID $id"
@@ -6,9 +7,15 @@ fi
 
 id=$(ps -ef | awk '/[t]omcat/{print $2}')
 if [ -z "$id" ]; then
-	echo "Tomcat not started. Starting it now:"
-	$CATALINA_HOME/bin/startup.sh
+	echo "Tomcat not started. Starting it now:"	
+	if [ -n "$1" ]; then
+		echo "Starting in debug-mode"
+		$CATALINA_HOME/bin/catalina.sh jpda start
+	else
+		$CATALINA_HOME/bin/startup.sh
+	fi
 fi
+
 
 mvn clean package 
 echo "Copying war to $CATALINA_HOME/webapps/"
